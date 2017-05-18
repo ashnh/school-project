9f435cd64ca9ae7fc9eea9d2820ada287da801bb
ashnh <ashnhx@gmail.com>
2017-05-18 12:06:36 -0400

    Update SideScroller.cs

--- Code/SideScroller.cs
+++ Code/SideScroller.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 using System.Collections;
 
 public class SideScroller : MonoBehaviour {
-
+	
+	public GameObject player;
+	
+	public GameObject platforms;
+	
+	public GameObject risingSediment;
+	
+	public float jumpSpeed;
+	public float runSpeed;
+	
+	void Start() {
+		
+	}
+	
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.W) && player.GetComponent <Collider2D> ().isTouching (platforms.GetComponent <Collider2D> ())) {
+			player.GetComponent <Rigidbody2D> ().velocity = new Vector2 (player.GetComponent <Rigidbody2D> ().velocity.x, jumpSpeed          );
+		}
+		
+		if (Input.GetKeyDown (KeyCode.A)) {
+			player.GetComponent <Rigidbody2D> ().velocity = new Vector2 (-runSpeed, player.GetComponent <Rigidbody2D> ().velocity.y          );
+		} else if (Input.GetKeyDown (KeyCode.D)) {
+			player.GetComponent <Rigidbody2D> ().velocity = new Vector2 (runSpeed, player.GetComponent <Rigidbody2D> ().velocity.y          );
+		}
+		
+		
+		
+	}
+	
 }
