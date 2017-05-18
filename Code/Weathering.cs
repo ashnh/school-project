e91d0611480bd84c741e20f03ef5e178ef9e643a
ashnh <ashnhx@gmail.com>
2017-05-18 11:52:57 -0400

    Update Weathering.cs

--- Code/Weathering.cs
+++ Code/Weathering.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class Weathering : MonoBehaviour {
 
 	public GameObject progressBar;
+	
+	public Material firstColor;
+	public Material secondColor;
+	public Material thirdColor;
 
 	public SpriteRenderer rock;
+	
+	public Sprite oneOfThree;
+	public Sprite twoOfThree;
+	public Sprite threeOfThree;
 
 	public GameObject popup;
 
 	bool finished;
-
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +33,17 @@ public class Weathering : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.F) && !finished) {
 			progressBar.transform.localScale = new Vector3 (progressBar.transform.localScale.x - 0.03F, progressBar.transform.localScale.y, progressBar.transform.localScale.z);
 		}
+		
+		if (progressBar.transform.localScale.x >= .66F) {
+			rock.sprite = oneOfThree;
+			progressBar.GetComponent <Renderer> ().material = firstColor;
+		} else if (progressBar.transform.localScale.x >= .66F) {
+			rock.sprite = twoOfThree;
+			progressBar.GetComponent <Renderer> ().material = secondColor;
+		} else {
+			rock.sprite = threeOfThree;
+			progressBar.GetComponent <Renderer> ().material = thirdColor;
+		}
 
 		if (progressBar.transform.localScale.x <= 0) {
 			finished = true;
@@ -33,6 +51,7 @@ public class Weathering : MonoBehaviour {
 
 		if (finished) {
 			popup.transform.localScale = new Vector3 (1, 1, 1);
+			Time.scale = 0;
 		}
 
 	}
